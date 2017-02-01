21f42d8f04a288da3582e7e062a562392d6625df
Fokke Zandbergen <mail@fokkezb.nl>
2017-02-01 09:25:34 +0100

    background app vs exit, closes #26

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,1 +1,11 @@
+if (OS_ANDROID) {
+    $.win.addEventListener('androidback', function () {
+        var intent = Ti.Android.createIntent({
+            action: Ti.Android.ACTION_MAIN
+        });
+        intent.addCategory(Ti.Android.CATEGORY_HOME);
+        Ti.Android.currentActivity.startActivity(intent);
+    });
+}
+
 $.index.open();
